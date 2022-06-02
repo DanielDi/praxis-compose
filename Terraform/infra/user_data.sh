236001b76e180a70b04a5b32feb3d31824aef709
DanielDi <despinalm@unal.edu.co>
2022-06-01 23:14:04 -0500

    Update user_data.sh

--- Terraform/infra/user_data.sh
+++ Terraform/infra/user_data.sh
@@ -31,6 +31,7 @@ curl -s https://deb.nodesource.com/setup_16.x | sudo bash
 sudo apt install nodejs -y
 
 ## Install cypress dependencies
+sudo apt-get install libc6 libpcre3
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 
 
